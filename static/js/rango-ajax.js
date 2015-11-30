bca79a217a4c1afe3a1f31e074cc7223684a4786
daya0576 <daya0576@gmail.com>
2015-11-30 21:40:58 +1100

    未登录时也显示课程点赞按钮，alert 登陆后点赞。

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -1,26 +1,3 @@
-// likes function
-$('#likes').click(function(){
-    var catid;
-    catid = $(this).attr("data-catid");
-    $.get('/rango/like_category/', {category_id: catid}, function(data){
-       $('#like_count').html(data);
-       $('#likes').hide();
-       $('#liked').show();
-       alert('thank u for your like~');
-    });
-});
-
-$('#liked').click(function(){
-    var catid;
-    catid = $(this).attr("data-catid");
-    $.get('/rango/dislike_category/', {category_id: catid}, function(data){
-       $('#like_count').html(data);
-       $('#liked').hide();
-       $('#likes').show();
-       alert('Hoping u can like me again~')
-    });
-});
-
 // search function
 $('#suggestion').keyup(function(){
     var query;
