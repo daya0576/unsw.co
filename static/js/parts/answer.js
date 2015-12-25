5ed1fe212a83ceafdaaa0c5859203718f0d5d402
daya0576 <daya0576@gmail.com>
2015-12-26 01:50:23 +1100

    添加百度统计

--- static/js/parts/answer.js
+++ static/js/parts/answer.js
@@ -18,7 +18,9 @@ function show_editor(obj, answer_id){
     $("#answer_content"+answer_id).html("<img scr='/images/favicon.ico'></img>");
     $.get('/rango/edit_description_view/', {answer_id: answer_id}, function(data){
 //        $("#answer_content"+answer_id).html(data);
-        $("#answer_content"+answer_id).load("/rango/edit_description_view/?answer_id=9");
+//        $("#answer_content"+answer_id).load("/rango/edit_description_view/?answer_id=9");
+        $("#answer_content"+answer_id).html('<script id="container" name="content" type="text/plain">这里写你的初始化内容</script>')
+j
     });
 
 }
