09140d4ed7f26d76aa2bf2e65d1ac7148c2a2abf
daya0576 <daya0576@gmail.com>
2016-01-02 12:39:13 +1100

    fix a bug

--- static/js/parts/facebook_app.js
+++ static/js/parts/facebook_app.js
@@ -1,17 +1,15 @@
-<script>
-  window.fbAsyncInit = function() {
+    window.fbAsyncInit = function() {
     FB.init({
       appId      : '1510020092635980',
       xfbml      : true,
       version    : 'v2.5'
     });
-  };
+    };
 
-  (function(d, s, id){
+    (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+    }(document, 'script', 'facebook-jssdk'));
