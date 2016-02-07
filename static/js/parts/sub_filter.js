4169a34bb3e64aa7bcc6b2f1f4342e971caaba3c
daya0576 <daya0576@gmail.com>
2016-02-07 14:55:02 +1100

    search button 交互

--- static/js/parts/sub_filter.js
+++ static/js/parts/sub_filter.js
@@ -30,8 +30,13 @@ function show_filter_search(){
     if($(".filter_search_box").is(':hidden')){
         $(".filter_search_box").show();
         $("#sub_keyword").focus();
+        $("#filter_search_switch").addClass("active");
+
+//        $("#filter_search_switch").css("background-color", "#d4d4d4")
     }else{
         $(".filter_search_box").hide();
+        $("#filter_search_switch").removeClass("active");
+        $("#filter_search_switch").css("background-color", "white")
     }
 
 }
