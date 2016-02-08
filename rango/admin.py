b656530d8d66a5fe5afa31352921b3bbf7b413bc
daya0576 <daya0576@gmail.com>
2016-02-08 22:07:59 +1100

    admin modify + AnswerUserDislikes + AnswerUserLikes

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from rango.models import Category, CatPage, Answers
+from rango.models import Category, CatPage, Answers, AnswerUserLikes, AnswerUserDislikes
 #, UserProfile
-
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -17,7 +16,18 @@ class AnswersAdmin(admin.ModelAdmin):
     list_display = ('post_date', 'author', 'content', 'category')
 
 
+class AnswerUserLikesAdmin(admin.ModelAdmin):
+    list_display = ('answer', 'user', 'time')
+
+
+class AnswerUserDislikesAdmin(admin.ModelAdmin):
+    list_display = ('answer', 'user', 'time')
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(CatPage, PageAdmin)
 admin.site.register(Answers, AnswersAdmin)
+admin.site.register(AnswerUserLikes, AnswerUserLikesAdmin)
+admin.site.register(AnswerUserDislikes, AnswerUserDislikesAdmin)
+
 # admin.site.register(UserProfile)
