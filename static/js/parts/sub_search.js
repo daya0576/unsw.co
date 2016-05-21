60843e7892b7369d7affa43ff99c5c1f4207551b
daya0576 <daya0576@gmail.com>
2016-05-21 21:02:58 +1000

    search loading...

--- static/js/parts/sub_search.js
+++ static/js/parts/sub_search.js
@@ -7,8 +7,11 @@ $('#sub_keyword').keyup(function(){
     var query;
     query = $(this).val();
     var sub_name_slug = $("#sub_name_slug").val();
+
+    $("#sub_search_loading").show();
 //    $('#nav_content').html("<i class='fa fa-refresh fa-spin fa-fw'></i>");
     $.get('/rango/sub_search/', {suggestion: query, sub_name_slug: sub_name_slug}, function(data){
+        $("#sub_search_loading").hide();
         $('#sub_content').html(data);
     });
 });
