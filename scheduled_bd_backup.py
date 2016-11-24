3f7b5175f339062a5eff32e2c7851b8289e488d4
daya0576 <daya0576@gmail.com>
2016-11-24 21:58:24 +1100

    backup

---
+++ scheduled_bd_backup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+This script creates a timestamped database backup,
+and cleans backups older than a set number of dates
+
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import sqlite3
+import shutil
+import time
+import os
+
+DESCRIPTION = """
+              Create a timestamped SQLite database backup, and
+              clean backups older than a defined number of days
+              """
+
+# How old a file needs to be in order
+# to be considered for being removed
+NO_OF_DAYS = 7
+
+def sqlite3_backup(dbfile, backupdir):
+    """Create timestamped database copy"""
+
+    if not os.path.isdir(backupdir):
+        raise Exception("Backup directory does not exist: {}".format(backupdir))
+
+    backup_file = os.path.join(backupdir, os.path.basename(dbfile) +
+                               time.strftime("-%Y%m%d-%H%M%S"))
+
+    connection = sqlite3.connect(dbfile)
+    cursor = connection.cursor()
+
+    # Lock database before making a backup
+    cursor.execute('begin immediate')
+    # Make new backup file
+    shutil.copyfile(dbfile, backup_file)
+    print ("\nCreating {}...".format(backup_file))
+    # Unlock database
+    connection.rollback()
+
+def clean_data(backup_dir):
+    """Delete files older than NO_OF_DAYS days"""
+
+    print ("\n------------------------------")
+    print ("Cleaning up old backups")
+
+    for filename in os.listdir(backup_dir):
+        backup_file = os.path.join(backup_dir, filename)
+        if os.stat(backup_file).st_ctime < (time.time() - NO_OF_DAYS * 86400):
+            if os.path.isfile(backup_file):
+                os.remove(backup_file)
+                print ("Deleting {}...".format(backup_file))
+
+def get_arguments():
+    """Parse the commandline arguments from the user"""
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('db_file',
+                        help='the database file that needs backed up')
+    parser.add_argument('backup_dir',
+                         help='the directory where the backup'
+                              'file should be saved')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_arguments()
+    sqlite3_backup(args.db_file, args.backup_dir)
+    clean_data(args.backup_dir)
+
+    print ("\nBackup update has been successful.")
--- static/z_test/wings_vote/vote_count.py
+++ static/z_test/wings_vote/vote_count.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     # exit()
 
     while 1:
-        time.sleep(60 * 30)
+        # time.sleep(60 * 30)
         html_result = get_html(vote_url)
         # html_result = "myChart_bing_8.setDataXML(<chart baseFontSize='12' numberSuffix='%' showAboutMenuItem='0' decimals='2' chartTopMargin='0' chartBottomMargin='0' caption='' showValues='0' formatNumberScale='0' bgAlpha='0' startingAngle='60'><set label='1' value='59.6' displayValue='WINGS战队（电竞） (59.6%)' toolText=' (59.6%)' isSliced='1' /><set label='2' value='24.5' displayValue='邹市明（拳击） (24.5%)' toolText=' (24.5%)'/><set label='3' value='7.6' displayValue='丁俊晖（台球） (7.6%)' toolText=' (7.6%)'/><set label='4' value='5.7' displayValue='柯洁（围棋） (5.7%)' toolText=' (5.7%)'/><set label='5' value='2.6' displayValue='张弛（轮滑） (2.6%)' toolText=' (2.6%)'/></chart>"
 
